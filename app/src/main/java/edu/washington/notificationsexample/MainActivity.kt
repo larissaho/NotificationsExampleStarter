1748b538462d406f82ffe075e0c86adb467a1bd4
alexishiniker <alexisr@uw.edu>
2019-11-12 09:43:50 -0800

    Add files via upload

---
+++ app/src/main/java/edu/washington/notificationsexample/MainActivity.kt
@@ -0,0 +1,81 @@
+package edu.washington.notificationsexample
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Context.NOTIFICATION_SERVICE
+import androidx.core.content.ContextCompat.getSystemService
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import android.graphics.BitmapFactory
+import androidx.core.app.ComponentActivity.ExtraData
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.graphics.createBitmap
+
+enum class NotificationID {
+    BASIC,
+    AIRPLANE
+}
+
+class MainActivity : AppCompatActivity() {
+    var totalNotifications = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    fun displayNotificationSimple(v: View) {
+        val builder = buildBasicNotification()
+        showBasicNotification(builder)
+    }
+
+    fun displayNotificationExpanded(v: View) {
+
+        // Build and show a notification with extra content
+    }
+
+    fun displayNotificationWithActions(v: View) {
+        // Build and show a notification with action buttons that can
+        // launch MainActivity and SecondActivity
+
+        // Add an action button that can reset the notification count
+    }
+
+    private fun addNotificationsForAirplaneMode() {
+
+        // Create a standalone BroadcastReceiver that can listen for changes to airplane mode
+        // Instantiate and register your BroadcastReceiver here
+        // Action: "android.intent.action.AIRPLANE_MODE"
+    }
+
+    private fun buildBasicNotification(): NotificationCompat.Builder {
+        totalNotifications++
+
+        var builder = NotificationCompat.Builder(this, "basic")
+            .setSmallIcon(android.R.drawable.ic_menu_add)
+            .setContentTitle("Title")
+            .setContentText(totalNotifications.toString())
+            .setAutoCancel(true)
+
+        return builder
+    }
+
+    private fun showBasicNotification(builder: NotificationCompat.Builder) {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NotificationID.BASIC.ordinal, builder.build())
+    }
+}
---
+++ app/src/main/java/edu/washington/notificationsexample/SecondActivity.kt
@@ -0,0 +1,17 @@
+package edu.washington.notificationsexample
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        setSupportActionBar(toolbar)
+    }
+
+}
